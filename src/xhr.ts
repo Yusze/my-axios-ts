5833cb3cdf9ea51d597caa2a0981ced018a40e0b
yusze <cyusze18@163.com>
2022-11-06 20:57:52 +0800

    improvement: improvement

---
+++ src/xhr.ts
@@ -0,0 +1,69 @@
+import { createError } from './helper/error'
+import { parseHeaders } from './helper/headers'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
+
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { url, data = null, method = 'GET', headers, responseType, timeout } = config // 赋默认值
+
+    const request = new XMLHttpRequest()
+    if (responseType) {
+      request.responseType = responseType
+    }
+    if (timeout) {
+      request.timeout = timeout
+    }
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+      if (request.status === 0) {
+        return
+      }
+      const responseHeader = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeader,
+        config,
+        request
+      }
+      handleResponse(response)
+    }
+
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request))
+    }
+
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        createError(
+          `Request falied with Status code ${response.status}`,
+          config,
+          null,
+          request,
+          response
+        )
+      }
+    }
+  })
+}
